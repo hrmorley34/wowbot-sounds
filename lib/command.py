b15edecd4e25f15c27f2a490f904d3eee7d6e9a2
hrmorley34 <henry@morley.org.uk>
2021-11-13 22:17:33 +0000

    [lib] Make BaseSlashCommand more easily subclassable

--- lib/command.py
+++ lib/command.py
@@ -1,4 +1,5 @@
-from typing import Dict, Optional, Set, cast
+from enum import Enum
+from typing import Dict, Optional, Set, Type, cast
 
 from .sound import Sound, SoundDict
 from .typing import CommandAliasName, CommandDef, CommandKwargType, CommandName, SlashCommandCommon, SlashCommandDef, SlashCommandOptionDef, SlashGroup, SlashName, SlashOption
@@ -24,25 +25,36 @@ class Command:
         self.commandkwargs = data.get("commandkwargs", {})
 
 
+class SlashCommandType(Enum):
+    normal = "normal"
+    options = "options"
+
+
 class BaseSlashCommand:
     name: SlashName
     group: Optional[SlashGroup]
     description: Optional[str]
+
+    _slashcommandtypes: Dict[SlashCommandType, Type["BaseSlashCommand"]] = {}
 
     def __new__(cls, group: Optional[SlashGroup], name: SlashName, data: SlashCommandCommon, sounds: SoundDict):
         if cls is not BaseSlashCommand:
             return object.__new__(cls)
 
         if "options" in data:
-            return SlashOptionCommand(group, name, cast(SlashCommandOptionDef, data), sounds=sounds)
+            return cls._slashcommandtypes[SlashCommandType.options](group, name, cast(SlashCommandOptionDef, data), sounds=sounds)
         else:
-            return SlashCommand(group, name, data, sounds=sounds)
+            return cls._slashcommandtypes[SlashCommandType.normal](group, name, data, sounds=sounds)
+
+    def __init_subclass__(cls, *, slashtype: Optional[SlashCommandType] = None):
+        if slashtype is not None:
+            cls._slashcommandtypes[slashtype] = cls
 
     def __init__(self, group: Optional[SlashGroup], name: SlashName, data: SlashCommandCommon, sounds: SoundDict):
         raise NotImplementedError
 
 
-class SlashCommand(BaseSlashCommand):
+class SlashCommand(BaseSlashCommand, slashtype=SlashCommandType.normal):
     sound: Sound
 
     def __init__(self, group: Optional[SlashGroup], name: SlashName, data: SlashCommandDef, sounds: SoundDict):
@@ -56,7 +68,7 @@ class SlashCommand(BaseSlashCommand):
         self.sound = sounds[data["sound"]]
 
 
-class SlashOptionCommand(BaseSlashCommand):
+class SlashOptionCommand(BaseSlashCommand, slashtype=SlashCommandType.options):
     options: Dict[SlashOption, Sound]
     default: SlashOption
 
